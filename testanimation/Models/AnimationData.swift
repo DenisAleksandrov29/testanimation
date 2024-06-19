5ad04e0f97c7013a374161a09f6cbf39495b6a82
DenisAlexandrov <aie-dinis2007@yandex.ru>
2024-06-19 11:44:39 +0300

    final

--- testanimation/Models/AnimationData.swift
+++ testanimation/Models/AnimationData.swift
@@ -8,20 +8,20 @@
 import Foundation
 
 struct AnimationData {
-    static let animations = ["pop", "slideLeft", "slideRight", "slideDown", "slideUp",
-                             "squeezeLeft", "squeezeRight", "squeezeDown", "squeezeUp",
-                             "fadeIn", "fadeOut", "fadeOutIn", "fadeInLeft",
-                             "fadeInRight", "fadeInDown", "fadeInUp", "zoomIn",
-                             "zoomOut", "fall", "shake", "flipX", "flipY", "morph",
-                             "squeeze", "flash", "wobble", "swing"]
+    static let animations = [
+        "pop", "slideLeft", "slideRight", "slideDown", "slideUp",
+        "squeezeLeft", "squeezeRight", "squeezeDown", "squeezeUp",
+        "fadeIn", "fadeOut", "fadeOutIn", "fadeInLeft","fadeInRight",
+        "fadeInDown", "fadeInUp", "zoomIn","zoomOut", "fall", "shake", "flipX",
+        "flipY", "morph", "squeeze", "flash", "wobble", "swing"]
     
-    static let curves = ["easeOut", "easeInOut", "linear", "spring", "easeInSine",
-                         "easeInSine", "easeOutSine", "easeInOutSine", "easeInQuad",
-                         "easeOutQuad", "easeInOutQuad", "easeInCubic", "easeOutCubic",
-                         "easeInOutCubic", "easeInQuart", "easeOutQuart", "easeInOutQuart",
-                         "easeInQuint", "easeOutQuint", "easeInOutQuint", "easeInOutQuint",
-                         "easeInExpo", "easeOutExpo", "easeInOutExpo", "easeInCirc",
-                         "easeInCirc", "easeOutCirc", "easeInOutCirc", "easeInBack",
-                         "easeOutBack", "easeInOutBack"]
+    static let curves = [
+        "easeOut", "easeInOut", "linear", "spring", "easeInSine","easeInSine",
+        "easeOutSine", "easeInOutSine", "easeInQuad","easeOutQuad", "easeInOutQuad",
+        "easeInCubic", "easeOutCubic","easeInOutCubic", "easeInQuart", "easeOutQuart",
+        "easeInOutQuart","easeInQuint", "easeOutQuint", "easeInOutQuint", "easeInOutQuint",
+        "easeInExpo", "easeOutExpo", "easeInOutExpo", "easeInCirc","easeInCirc",
+        "easeOutCirc", "easeInOutCirc", "easeInBack","easeOutBack", "easeInOutBack"]
 }
+
 
